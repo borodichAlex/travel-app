e50e91f627fc7fb48855b53828d999bc42f1e25c
GrootStudent <borodichalexander97@gmail.com>
2021-03-11 17:25:55 +0300

    feat: create client routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Travel app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <header>header</header>
+
+        <main>
+        <Switch>
+          <Route exact path="/">
+            <h2>Grid Cards</h2>
+          </Route>
+          <Route path="/country/:id">
+            <h2>Country</h2>
+          </Route>
+          <Route path="/404">
+            <h2>404</h2>
+          </Route>
+          <Redirect to="404" />
+        </Switch>
+        </main>
+
+        <footer>footer</footer>
+      </div>
+    </Router>
   );
 }
 
