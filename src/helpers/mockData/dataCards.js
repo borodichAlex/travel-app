b8f110b476da005e138f5903af08bafa8878e066
GrootStudent <borodichalexander97@gmail.com>
2021-03-11 17:51:18 +0300

    feat: add mock data for cards

---
+++ src/helpers/mockData/dataCards.js
@@ -0,0 +1,51 @@
+export const dataCards = [
+  {
+    id: 1,
+    name: 'Canada',
+    capital: 'Ottawa',
+    imgUrl: 'url',
+  },
+  {
+    id: 2,
+    name: 'Russia',
+    capital: 'Moscow',
+    imgUrl: 'url',
+  },
+  {
+    id: 3,
+    name: 'England',
+    capital: 'London',
+    imgUrl: 'url',
+  },
+  {
+    id: 4,
+    name: 'Australia',
+    capital: 'Canberra',
+    imgUrl: 'url',
+  },
+  {
+    id: 5,
+    name: 'Ireland',
+    capital: 'Dublin',
+    imgUrl: 'url',
+  },
+  {
+    id: 6,
+    name: 'France',
+    capital: 'Paris',
+    imgUrl: 'url',
+  },
+  {
+    id: 7,
+    name: 'Spain',
+    capital: 'Madrid',
+    imgUrl: 'url',
+  },
+  {
+    id: 8,
+    name: 'Italy',
+    capital: 'Roma',
+    imgUrl: 'url',
+  }
+
+];
