5da72f41ba5d0d3a74a626cc37275813d2c4827e
GrootStudent <borodichalexander97@gmail.com>
2021-03-11 17:52:09 +0300

    feat: create basic structure for main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import GridCards from './components/GridCards/GridCards';
+import { composeMultiple } from './helpers/composeMultiple';
+import { ListWithLink } from './components/ListWithLinks/ListWithLinks';
+import { ListCardsCountries } from './components/ListCardsCountries/ListCardsCountries';
+import { dataCards as mockDataCards } from './helpers/mockData/dataCards';
+
 import './App.css';
 
 function App() {
@@ -8,18 +14,20 @@ function App() {
         <header>header</header>
 
         <main>
-        <Switch>
-          <Route exact path="/">
-            <h2>Grid Cards</h2>
-          </Route>
-          <Route path="/country/:id">
-            <h2>Country</h2>
-          </Route>
-          <Route path="/404">
-            <h2>404</h2>
-          </Route>
-          <Redirect to="404" />
-        </Switch>
+          <Switch>
+            <Route exact path="/">
+              <GridCards>
+                {composeMultiple(ListWithLink, ListCardsCountries)(mockDataCards)}
+              </GridCards>
+            </Route>
+            <Route path="/country/:id">
+              <h2>Country</h2>
+            </Route>
+            <Route path="/404">
+              <h2>404</h2>
+            </Route>
+            <Redirect to="404" />
+          </Switch>
         </main>
 
         <footer>footer</footer>
