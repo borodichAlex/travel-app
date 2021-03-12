c1e9f9536ed3670cc59932619bee4c6b25f18cac
GrootStudent <borodichalexander97@gmail.com>
2021-03-12 13:52:06 +0300

    feat: add interfaces for json

---
+++ src/interfaces.ts
@@ -0,0 +1,43 @@
+interface ICountry {
+  id: number;
+  imageUrl: IImageUrl;
+  location: ILocation;
+  currency: ICurrency;
+  localizations: ILocalization[];
+  ISOCode: string;
+}
+
+interface ILocalization {
+  lang: string;
+  name: string;
+  capital: string;
+  description: string;
+  currency: {
+    fullName: string;
+  }
+}
+
+interface ICurrency {
+  name: string;
+  symbol: string;
+}
+
+interface ILocation {
+  coordinates: ICoordinates;
+  timezone: string;
+}
+
+interface ICoordinates {
+  lat: number;
+  lon: number;
+}
+
+interface IImageUrl {
+  large: string;
+  small: string;
+}
+
+export type {
+  ICountry,
+  ICoordinates,
+}
